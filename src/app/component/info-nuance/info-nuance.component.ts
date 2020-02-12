57e2790984e764ad0c44deb5b2ed71630349221d
Valentin C <valentin.coulon@icloud.com>
2020-02-12 12:59:26 +0100

    tbl fait manque à faire le GetDiamFromGrade

--- src/app/component/info-nuance/info-nuance.component.ts
+++ src/app/component/info-nuance/info-nuance.component.ts
@@ -1,48 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { ArmaServConnection } from 'src/app/classes/info-connection';
-import { HttpClient } from '@angular/common/http';
-import { MatTableDataSource } from '@angular/material/table';
+import { Component, OnInit } from "@angular/core";
+import { ArmaServConnection } from "src/app/classes/info-connection";
+import { HttpClient } from "@angular/common/http";
+import { MatTableDataSource } from "@angular/material/table";
 
 @Component({
-  selector: 'app-info-nuance',
-  templateUrl: './info-nuance.component.html',
-  styleUrls: ['./info-nuance.component.css']
+  selector: "app-info-nuance",
+  templateUrl: "./info-nuance.component.html",
+  styleUrls: ["./info-nuance.component.css"]
 })
 export class InfoNuanceComponent implements OnInit {
-
-  result: any[] ;
-  dataSource: any[] ;
-  displayedColumns ;
-
-  constructor(private http: HttpClient) { 
-     
-  }
+  result: any[];
+  dataSource: any[];
+  displayedColumns;
+  diams: Array<object> = [];
+  
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    ArmaServConnection.GET(this.http, "GetAllGrade", [])
-    .subscribe((res) => {
+    ArmaServConnection.GET(this.http, "GetAllGrade", []).subscribe(res => {
       this.result = res.result[0];
-      this.displayedColumns = ['Name'];
+      this.displayedColumns = ["Name", "ShortName", "Code", "Default"];
       this.dataSource = this.result;
-      console.log(this.dataSource)
-    })
 
+      for (let cd of this.result) {
+        ArmaServConnection.GET(this.http, "GetDiamFromGrade", [
+          cd.Code
+        ]).subscribe(details => {
+
+          this.result.push(details.result[0]);
+          console.log(this.result[0]);
+        });
+      }
+    });
+    console.log(this.diams);
   }
-
-
 }
+
 interface DiamLinearWeight {
-  Diam: string,
-  LinWeight: string
+  Diam: string;
+  LinWeight: string;
 }
 
 interface Grade {
-  Name: string,
-  ShortName: string,
-  Code: string,
-  Default: number
+  Name: string;
+  ShortName: string;
+  Code: string;
+  Default: number;
 }
 
-interface ArmaServResultInterface  {
-  result: any[] // Le result ici peut etre n'importequoi (DiamLinearWeight, Grade, ...)
+interface ArmaServResultInterface {
+  result: any[]; // Le result ici peut etre n'importequoi (DiamLinearWeight, Grade, ...)
 }
